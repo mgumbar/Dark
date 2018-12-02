48199862968832ff737551af946c0f8322c3c606
mgumbar <mgumbar@gmail.com>
2018-12-02 01:59:36 +0100

    mongo db

---
+++ Dark/Context/DarkContext.cs
@@ -0,0 +1,31 @@
+﻿using Dark.Models;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dark.Context
+{
+    public class DarkContext
+    {
+        private readonly IMongoDatabase _database = null;
+
+        public DarkContext(IOptions<Settings> settings)
+        {
+            var client = new MongoClient(settings.Value.ConnectionString);
+            if (client != null)
+                _database = client.GetDatabase(settings.Value.Database);
+        }
+
+        public IMongoCollection<Log> Logs
+        {
+            get
+            {
+                return _database.GetCollection<Log>("log");
+            }
+        }
+    }
+}
---
+++ Dark/Repository/ILogRepository.cs
@@ -0,0 +1,33 @@
+﻿using Dark.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dark.Repository
+{
+    public interface ILogRepository
+    {
+        Task<IEnumerable<Log>> GetAllLogs();
+
+        Task<Log> GetLog(string id);
+
+        // query after multiple parameters
+        Task<IEnumerable<Log>> GetLog(string bodyText, DateTime updatedFrom, long headerSizeLimit);
+
+        // add new note document
+        Task AddLog(Log item);
+
+        // remove a single document / note
+        Task<bool> RemoveLog(string id);
+
+        // update just a single document / note
+        Task<bool> UpdateLog(string id, string body);
+
+        // demo interface - full document update
+        //Task<bool> UpdateNoteDocument(string id, string body);
+
+        // should be used with high cautious, only in relation with demo setup
+        Task<bool> RemoveAllLogs();
+    }
+}
