036b4edbabcd11030244c40104a711824aa332f7
mgumbar <mgumbar@gmail.com>
2018-12-15 22:20:29 +0100

    move common Folder

---
+++ Dark/Common/DAL/Models/Worker.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Models
+{
+    [BsonIgnoreExtraElements]
+    class Worker
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public ObjectId Id { get; set; }
+        [BsonElement("date_time")]
+        [JsonProperty("date_time")]
+        public DateTime DateTime { get; set; }
+        [BsonElement("updated_at")]
+        [JsonProperty("updated_at")]
+        public DateTime UpdatedAt { get; set; }
+        [BsonElement("payload")]
+        //public Dictionary<string, string> Payload { get; set; }
+        public string Payload { get; set; }
+        [BsonElement("status")]
+        public string Satus { get; set; }
+        [BsonElement("job_name")]
+        public string JobName { get; set; }
+        [BsonElement("progress")]
+        public float Progress { get; set; }
+        [BsonElement("exception")]
+        public string Exception { get; set; }
+    }
+}
---
+++ Dark/Common/DAL/UnitOfWork.cs
@@ -0,0 +1,84 @@
+﻿using DAL.Context;
+using DAL.Models;
+using DAL.Repositories;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private DarkContext context;
+        private GenericRepository<Log> log;
+        private GenericRepository<Workflow> amon;
+        private GenericRepository<Worker> courseRepository;
+        private IOptions<Settings> settings;
+
+        public UnitOfWork(IOptions<Settings> settings)
+        {
+            this.context = new DarkContext(settings);
+            this.settings = settings;
+        }
+
+        public DarkContext Context
+        {
+            get
+            {
+                return this.context;
+            }
+        }
+
+        public IGenericRepository<Log> Log
+        {
+            get
+            {
+
+                if (this.log == null)
+                {
+                    this.log = new GenericRepository<Log>(settings);
+                }
+                return this.log;
+            }
+        }
+
+        public IGenericRepository<Workflow> Amon
+        {
+            get
+            {
+
+                if (this.amon == null)
+                {
+                    this.amon = new GenericRepository<Workflow>(settings);
+                }
+                return this.amon;
+            }
+        }
+
+        public void Save()
+        {
+            //context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    //context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
